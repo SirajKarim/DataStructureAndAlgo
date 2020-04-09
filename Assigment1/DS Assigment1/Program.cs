8fe7b7e20935e65f9b535baff2c0171d7a9166d0
SirajKarim <msirajakarim437@gmail.com>
2020-04-09 13:20:07 +0500

    Data structure Assigment 1

---
+++ Assigment1/DS Assigment1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Assigment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            double n = double.Parse(Console.ReadLine());
+            double val = 2 * n - 1;
+            Console.WriteLine(val);
+            double a = 1;
+            double fomu = (n / 2) * (a + val);
+            Console.WriteLine("Sum of the odd series is {0}", fomu);
+            Console.WriteLine("Q No:    02");
+            int[] arr = {50,40,30,20,10 };
+            int count = arr.Length-1;
+            Console.WriteLine("Unsorted Series");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" {0} ",arr[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Reverse Series");
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = arr[i] + arr[count];
+                arr[count] = arr[i] - arr[count];
+                arr[i] = arr[i] - arr[count];
+                count--;
+            }
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" {0} ",arr[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
