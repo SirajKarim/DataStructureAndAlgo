760b913c2b9cb7e5befe0d53f0442f0a59a77388
SirajKarim <msirajakarim437@gmail.com>
2020-04-10 18:13:07 +0500

    Data structure Assigment 2

---
+++ Assigment2/Assigment 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorting obj1 = new Sorting();
+            File obj = new File();
+            File[] arr = new File[2];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new File();
+                Console.WriteLine("enter name");
+                arr[i].name = Console.ReadLine();
+                Console.WriteLine("enter age");
+                arr[i].age = float.Parse(Console.ReadLine());
+            }
+
+
+           
+            Console.WriteLine("Select bubble sort or selectional sort");
+            Console.WriteLine("Press b for bubble sort or s for selectional sort");
+            string userPrompt = Console.ReadLine();
+            if (userPrompt=="b")
+            {
+                Console.WriteLine("Press 1 to sort by size and 2 for sort by name");
+                int userInput = int.Parse(Console.ReadLine());
+                if (userInput==1)
+                {
+                    obj1.bubbleSortSize(arr);
+                }
+                else if (userInput == 2)
+                {
+                    obj1.bubbleSortName(arr);
+                }
+            }
+            else if (userPrompt == "s")
+            {
+                obj1.selectionSort(arr);
+
+            }
+
+        }
+    }
+}
---
+++ Assigment2/Assigment 2/Sorting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_2
+{
+    class Sorting:File
+    {
+        
+        float temp1 = 0;
+        string temp;
+        public  void selectionSort(File [] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = i;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[min].age>arr[j].age)
+                    {
+                        min = j;
+                    }
+                }
+                temp1 = arr[min].age;
+                arr[min].age = arr[i].age;
+                arr[i].age = temp1;
+
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.age);
+            }
+        }
+        public  void bubbleSortName(File [] arr)
+        {
+            for (int i = arr.Length-1; i >0 ; i--)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j].name.CompareTo(arr[i].name)>0)
+                    {
+                        temp = arr[i].name;
+                        arr[i].name = arr[j].name;
+                        arr[j].name = temp;
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.name);
+            }
+        }
+        public void bubbleSortSize(File [] arr)
+        {
+           // int l = arr.Length;
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j].age>arr[i].age)
+                    {
+                        temp1 = arr[i].age;
+                        arr[i].age = arr[j].age;
+                        arr[j].age = temp1;
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.age);
+            }
+        }
+    }
+}
